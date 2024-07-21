2af775b33c9dde2f5226d3416cc7b4e2fe04295e
Luke Boswell <lukewilliamboswell@gmail.com>
2024-07-21 11:51:30 +1000

    simplify build script

--- build.sh
+++ build.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
+# This script is used to build and run the app locally.
+
+# In future I'd love to have a `--watch` command for rtl and basic-webserver
+# so we can just have this running in the background and insta load on any changes.
+#
+# It won't be too hard to implement, it just needs someone to do it.
+
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
 ROC="roc"
 
 # generate templates
-rm -rf src/Views/Pages.roc
-rtl -e "html" -i ./templates/ -o ./src/Views/ || true # currently a workaround for not being able to roc check from a parent
-(ls ./src/Views/Pages.roc >> /dev/null 2>&1 && exit)
-cd ./src/Views && $ROC check Pages.roc && cd ../..
+rtl -e "html" -i ./templates/ -o ./src/Views/
 
 # build app
-rm -rf src/app
-# $ROC build --optimize app.roc || true
-$ROC build src/app.roc || true
-(ls src/app >> /dev/null 2>&1 && exit)
+$ROC build src/app.roc
 
 # build css
-rm -rf www/app.css
-# tailwindcss -i site.css -o www/app.css --minify
 tailwindcss -i site.css -o www/app.css
 
 # clean test sqlite db
-rm -rf app.db
-sqlite3 app.db < app.sql
+rm -rf app.db && sqlite3 app.db < app.sql
 
 # start server
+# note we do this in a subshell so we can kill both the roc and simple-http-server processes
+# with a single ctrl-c, it just makes life easier
 cd www
 (trap 'kill 0' SIGINT; ../src/app & simple-http-server --port 8001 --cors)
