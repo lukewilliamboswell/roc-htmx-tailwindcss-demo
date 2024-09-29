5ccf751db41557702b18a03cecd07f84bdfc7b57
Luke Boswell <lukewilliamboswell@gmail.com>
2024-09-29 12:13:11 +1000

    ignore rtl for now

--- build.sh
+++ build.sh
@@ -2,16 +2,11 @@
 
 # This script is used to build and run the app locally.
 
-# In future I'd love to have a `--watch` command for rtl and basic-webserver
-# so we can just have this running in the background and insta load on any changes.
-#
-# It won't be too hard to implement, it just needs someone to do it.
-
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
 # generate templates
-rtl -e "html" -i ./templates/ -o ./src/Views/
+# rtl -e "html" -i ./templates/ -o ./src/Views/
 
 # build app
 roc build src/main.roc --output src/server
